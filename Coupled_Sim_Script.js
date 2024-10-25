8bb1cca575683bdba7b8b75de3e08dc5a2e898cc
ParraJesus <jgparra@unicauca.edu.co>
2024-10-24 20:33:27 -0500

    xx

--- Coupled_Sim_Script.js
+++ Coupled_Sim_Script.js
@@ -2,9 +2,9 @@
 let barra_l = 10;
 let barra_m = 0;
 let esfera_m = 0;
+let k0 = 0;
 let k1 = 0;
 let k2 = 0;
-let k3 = 0;
 
 //Datos Calculados
 let barraPosActual = 0, barraVelActual = 0, barraAceleActual = 0;
@@ -28,9 +28,9 @@ document.addEventListener('inputDataUpdated', function(e) {
     barra_m = parseFloat(inputData.barra_masa);
     barra_l = parseFloat(inputData.barra_longitud);
     esfera_m = parseFloat(inputData.esfera_masa);
-    k1 = parseFloat(inputData.k1);
-    k2 = parseFloat(inputData.k2);
-    k3 = parseFloat(inputData.k3);
+    k0 = parseFloat(inputData.k1);
+    k1 = parseFloat(inputData.k2);
+    k2 = parseFloat(inputData.k3);
 });
 
 document.addEventListener('startAnimation', function(e) {
@@ -172,19 +172,48 @@ function pauseAnimation()
     }
 }
 
-//document.getElementById('').innerHTML = ``; //Fórmula
-//document.getElementById('').innerHTML = ``; //Valor
+//document.getElementById('').innerHTML = ``; //Fórmula document.getElementById('').innerHTML = ``; //Valor
 
 function inicializarEcuaciones(){
+    //Variables
+    let I = 1/12*m*l**2;
+
+    let a = I*m;
+    let b = (-I*(k1 + k2) - m*(k0*l*l/4 + k1*l*l/4));
+    let c = ((k0*l*l/4 + k1*l*l/4)*(k1 + k2) - Math.pow(k1*l/2, 2));
+    
+    let omega1 = Math.sqrt((-b + Math.sqrt(b*b - 4*a*c))/(2*a));
+    let omega2 = Math.sqrt((-b - Math.sqrt(b*b - 4*a*c))/(2*a));
+
+
     //Lagrangriano
     document.getElementById('formula_lagrangiano').innerHTML = `L = (1/2)I(θ')² + (1/2)m(x')² - [(1/2)k₀(l/2θ)² + (1/2)k₁(l/2θ + x)² + (1/2)k₂x² + mgx]`; //Fórmula
-    document.getElementById('formula_evaluada_lagrangiano').innerHTML = ``; //Valor
+    document.getElementById('formula_evaluada_lagrangiano').innerHTML = `L = `; //Valor
 
     //Ecuaciones Diferenciales
-    document.getElementById('formula_ED_1').innerHTML = ``; //Fórmula
-    document.getElementById('formula_ED_2').innerHTML = ``; //Fórmula
+    document.getElementById('formula_ED_1').innerHTML = `I(θ'') + (k₀l²/4)θ + (k₁l²/4)θ + (k₁l/2)x = 0`; //Fórmula
+    document.getElementById('formula_ED_2').innerHTML = `m(x'') + (k₁l/2)θ + (k₁ + k₂)x + mg = 0`; //Fórmula
 
     document.getElementById('formula_evaluada_ED_1').innerHTML = ``; //valor
     document.getElementById('formula_evaluada_ED_1').innerHTML = ``; //Valor
     
+    //Frecuencias naturales
+    document.getElementById('formula_general_frecuencia_1').innerHTML = `aω⁴ + bω² + c = 0`; //Fórmula
+    
+    //Fórmula de a
+    document.getElementById('formula_coeficiente_a').innerHTML = `a = Lm`; //Fórmula 
+    document.getElementById('formula_evaluada_coeficiente_a').innerHTML = `a = `; //Valor
+
+    //Fórmula de b
+    document.getElementById('formula_coeficiente_b').innerHTML = `b = (-I(k₁ + k₂) - m[k₀l²/4 + k₁l²/4])`; //Fórmula 
+    document.getElementById('formula_evaluada_coeficiente_b').innerHTML = `b = `; //Valor
+
+    //Fórmula de c
+    document.getElementById('formula_coeficiente_c').innerHTML = `c = [k₀l²/4 + k₁l²/4](k₁ + k₂) - (k₁l/2)²`; //Fórmula 
+    document.getElementById('formula_evaluada_coeficiente_c').innerHTML = `c = `; //Valor
+
+    //Fórmula w1 y w2
+    document.getElementById('formula_evaluada_frecuencia_1').innerHTML = `${omega1}`; //Valor
+    document.getElementById('formula_evaluada_frecuencia_2').innerHTML = `${omega2}`; //Valor
+
 }
